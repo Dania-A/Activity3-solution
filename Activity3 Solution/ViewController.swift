fa72a23ff7436f5417948a491e38242e26dedca7
Dania A <dania@danias-macbook-pro.local>
2018-10-20 21:02:32 +0300

    Initial Commit

---
+++ Activity3 Solution/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Activity3 Solution
+//
+//  Created by Dania A on 20/10/2018.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
